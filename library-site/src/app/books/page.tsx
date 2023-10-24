5fbdc7a0eaaa98c75d01aecfe8f26c6b2e2cde5d
Tom Mullier <tom.mullier@outlook.fr>
2023-10-24 21:45:09 +0200

    Books : Global Layout | Books Layout beginning | Raw Data for tests

--- library-site/src/app/books/page.tsx
+++ library-site/src/app/books/page.tsx
@@ -2,6 +2,7 @@
 
 import { FC, ReactElement, useEffect } from 'react';
 import { useBooksProviders } from '@/hooks';
+import 'flowbite';
 
 const BooksPage: FC = (): ReactElement => {
   const { useListBooks } = useBooksProviders();
@@ -11,30 +12,79 @@ const BooksPage: FC = (): ReactElement => {
 
   return (
     <>
-      <h1>Books</h1>
+      <section className="layout_book">
+        <section className="left_side_book">
+          <div className="top_section">
+            <div className="text">
+              <h1 className="welcome">Hello Xavier!</h1>
+              <p>Selection of the best books, just for you</p>
+              <a href="/" className="latest">
+                Show latest
+              </a>
+            </div>
+            <div className="image_ bg-book_fly bg-contain bg-right bg-no-repeat" />
+          </div>
+          <div className="books_container">
+            <div className="filter_container">
+              <div className="filter_title">Filter by :</div>
+              <div className="filter_item active">Title</div>
+              <div className="filter_item">Author</div>
+              <div className="filter_item">Category</div>
+              <div className="filter_item">Rating</div>
+            </div>
+            <div className="books_list">
+              {books.map((book) => (
+                <div className="book_item">
+                  <div className="book_image bg-book_cover bg-cover bg-center bg-no-repeat" />
+                  <div className="book_info">
+                    <div className="book_title">Titre du Livre</div>
+                    <div className="book_author">Tom Mullier</div>
+                    <div className="book_category">Thriller, Policier</div>
+                    <div className="book_rating">
+                      <i className="fa-solid fa-star" />
+                      <i className="fa-solid fa-star" />
+                      <i className="fa-solid fa-star" />
+                      <i className="fa-solid fa-star" />
+                      <i className="fa-solid fa-star" />
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </section>
+        <section className="right_side_book">
+          <h1>Your library</h1>
+        </section>
+      </section>
+      {/* BOOKS */}
+
       <div className="flex" style={{ width: 'auto', height: 'auto' }}>
-        {books.map((book) => (
+        {/* {books.map((book) => (
           <div
             key={book.id}
-            className="flex  flex-col m-10 book_card rounded shadow-lg h-24 bg-back_card bg-cover"
+            className="flex rounded-xl flex-col m-10 book_card rounded overflow-hidden shadow-lg h-24 bg-back_card bg-cover from-orange-400 via-red-500 to-pink-500"
             style={{ width: '300px', height: '424px' }}
           >
-            <div className="book_card__title font-bold text-xl mb-4 p-4">
+            <div className="book_card__title font-bold text-xl mb-2">
               {book.name}
             </div>
-            <div className="book_card__author p-4">
-              {book.author.firstName}
-              {book.author.lastName}
-              (author)
+            <div className="book_card__author">
+              {book.id}
+              {book.id}
             </div>
-            {/* Bottom right tailwind */}
-            <div className="book_card__actions flex justify-end w-m bg-none bottom-0 content-end h-full items-end">
-              <button type="button" className="book_card__action text-blue_1">
-                R
+            <div className="book_card__actions flex justify-end w-m bg-none bottom-0 content-end h-full items-end ">
+              <button
+                data-modal-toggle="modalBookDesc"
+                data-modal-target="modalBookDesc"
+                type="button"
+                className="book_card__action text-blue_1"
+              >
+                <i className="fa-solid fa-file-lines" />
               </button>
             </div>
           </div>
-        ))}
+        ))} */}
       </div>
     </>
   );
--- library-site/src/app/layout.tsx
+++ library-site/src/app/layout.tsx
@@ -17,7 +17,52 @@ export default function RootLayout({
 }): ReactElement {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <title>Library</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <script src="https://kit.fontawesome.com/b89837f3ad.js" /> {/*eslint-disable-line*/}
+      <body className={inter.className}>
+        <div className="left_side">
+          <div className="website_title">
+            <i className="fas fa-book" />
+            <h1>Library</h1>
+          </div>
+          <div className="menu">
+            <div className="menu_item">
+              <i className="fas fa-home" />
+              <a href="/">Home</a>
+            </div>
+            <div className="menu_item">
+              <i className="fas fa-book" />
+              <a href="/books">Books</a>
+            </div>
+            <div className="menu_item">
+              <i className="fas fa-user" />
+              <a href="/authors">Authors</a>
+            </div>
+            <div className="menu_item">
+              <i className="fas fa-users" />
+              <a href="/users">Utilisateurs</a>
+            </div>
+          </div>
+        </div>
+        <div className="right_side">
+          <div className="top_bar">
+            <input
+              type="text"
+              name="search"
+              id="search_book"
+              placeholder="Search"
+            />
+            <div className="right_pp">
+              <div className="pp bg-people bg-cover bg-center" />
+              <div className="pp_name">
+                <h3>Xavier Dupont</h3>
+              </div>
+            </div>
+          </div>
+          <div className="content">{children}</div>
+        </div>
+      </body>
     </html>
   );
 }
