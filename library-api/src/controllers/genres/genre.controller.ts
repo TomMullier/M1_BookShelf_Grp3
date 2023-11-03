03a82aa549ab6de621df364c71b40ee811257c7c
MaximeD <maxime.declemy@student.junia.com>
2023-11-03 17:36:12 +0100

    feat(Genre): find genre by id working

--- library-api/src/controllers/genres/genre.controller.ts
+++ library-api/src/controllers/genres/genre.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { GenreUseCases } from 'library-api/src/useCases';
+import { GenreId } from 'library-api/src/entities';
 import { GenrePresenter } from './genre.presenter';
 import { CreateGenreDto } from './genre.dto';
 
@@ -12,6 +13,12 @@ export class GenreController {
     const genres = await this.genreUseCases.getAll();
 
     return genres.map(GenrePresenter.from);
+  }
+
+  @Get('/:id')
+  public async getById(@Param('id') id: GenreId): Promise<GenrePresenter> {
+    const genre = await this.genreUseCases.getById(id);
+    return GenrePresenter.from(genre);
   }
 
   @Post('/')
--- library-api/src/useCases/genres/genre.useCases.ts
+++ library-api/src/useCases/genres/genre.useCases.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { GenreRepository } from 'library-api/src/repositories';
+import { GenreId } from 'library-api/src/entities';
 import {
   CreateGenreUseCasesInput,
   GenreUseCasesOutput,
@@ -15,6 +16,16 @@ export class GenreUseCases {
    */
   public async getAll(): Promise<GenreUseCasesOutput[]> {
     return this.genreRepository.getAll();
+  }
+
+  /**
+   * Get a genre by its ID
+   * @param id Genre's ID
+   * @returns Genre if found
+   * @throws 404: genre with this ID was not found
+   */
+  public async getById(id: GenreId): Promise<GenreUseCasesOutput> {
+    return this.genreRepository.getById(id);
   }
 
   /**
